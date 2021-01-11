398d9f320bd7c933a5969f2a3a92a98ba366ceaf
Patrick-Wynn-SewIY <77075239+Patrick-Wynn-SewIY@users.noreply.github.com>
2021-01-11 17:31:33 -0500

    Add files via upload

---
+++ SewIY-Education-Kit.ino
@@ -0,0 +1,69 @@
+/*********************************************************
+  This is a library for the MPR121 12-channel Capacitive touch sensor
+
+  Designed specifically to work with the MPR121 Breakout in the Adafruit shop
+  ----> https://www.adafruit.com/products/
+
+  These sensors use I2C communicate, at least 2 pins are required
+  to interface
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+
+  Modded by Jayson Owens/ja450n to include TinyWire support for ATtiny Boards
+  
+  Use TinyWireM.h for ATtiny's, otherwise stick with Wire.h
+
+  IMPORTANT - If using ATtiny you won't be able to use the
+  hardware Serial functions, so you'll need to comment out or remove
+  the Serial.x calls in the example code below
+
+  Modded by Patrick Wynn/Patrick-Wynn to work kit developed by SewIY
+
+**********************************************************/
+
+#include <TinyWireM.h>
+#include "Adafruit_MPR121.h"
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+#define LED_PIN PB4
+#define LED_COUNT 6
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  strip.begin();
+  strip.show();
+  strip.setBrightness(30); //set brighness low so batteries last
+  cap.begin(0x5A);//I2C address for MRP121
+}
+
+void loop() {
+  currtouched = cap.touched();
+  if (cap.touched() & (1 << 4)) {
+    strip.setPixelColor(0, strip.Color(0, 255, 255)); //led num,r,g,b
+    strip.setPixelColor(1, strip.Color(0, 255, 255));
+    strip.setPixelColor(2, strip.Color(0, 255, 255));
+    strip.setPixelColor(3, strip.Color(0, 255, 255));
+
+    strip.show();
+  }
+  if (! (cap.touched() & (1 << 4)) ) {
+    strip.clear();
+    strip.show();
+  }
+  lasttouched = currtouched;
+}
